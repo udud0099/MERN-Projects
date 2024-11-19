7fd83854a88d0d941a736d956337d7c3c5be9fb9
udud0099 <udud0099@gmail.com>
2024-11-19 21:09:13 +0545

    food delivery - v 1h:02:00 home page view menu working

--- Food Delivery/frontend/src/components/Header/Header.jsx
+++ Food Delivery/frontend/src/components/Header/Header.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header">
+        <div className="header-contents">
+          <h2>Order your favourite food here</h2>
+          <p>
+            Choose from a diverse menu featuring a delectable array of dishes
+            crafted with the finest ingredients and culinary experitise. Our
+            mission is to satisfy your cravings and elevate your dining
+            experience, one delicious meal at a time.
+          </p>
+          <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
