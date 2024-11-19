80e44b9337af2c1e3d2ef318312e56267bbf6ff2
udud0099 <udud0099@gmail.com>
2024-11-19 20:18:30 +0545

    Food Delivery v40M:00 navbar and basick hove page and other pages make

--- Food Delivery/frontend/src/App.jsx
+++ Food Delivery/frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
